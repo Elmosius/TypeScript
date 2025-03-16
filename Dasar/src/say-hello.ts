7c1c7a0f36ae4208d25dd4dbf334563cd4ffe9af
Elmosius <2272008@maranatha.ac.id>
2025-03-16 15:39:06 +0700

    coba cek data primitif

--- Dasar/src/say-hello.ts
+++ Dasar/src/say-hello.ts
@@ -1,3 +1,5 @@
-export function sayHello(name: String): String {
+export function sayHello(name: string): string {
+
     return `Hello ${name}`;
+
 }
---
+++ Dasar/test/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("data type", () => {
+  it("should must declare", () => {
+    const name: string = "John Doe";
+    const balance: number = 1000;
+    const isVip: boolean = true;
+
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+  });
+});
