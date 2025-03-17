f22ab2004e69be5d1eb33492f03e1a2deb79e53e
Elmosius <2272008@maranatha.ac.id>
2025-03-17 21:38:59 +0700

    cobain extends interface

---
+++ Dasar/test/interface.test.ts
@@ -0,0 +1,69 @@
+import { Seller } from "../src/seller";
+import { Employee, Manager } from "../src/employee";
+
+describe("Interface", () => {
+  it("should support in typescript", () => {
+    const seller: Seller = {
+      id: 1,
+      name: "John Doe",
+      address: "Jl. Raya",
+      phone: "08123456789",
+      email: "john@gmail.com",
+    };
+
+    // seller.phone = "123";  // gabisa karena readonly
+    console.info(seller);
+  });
+
+  it("should support function interface in typescript", () => {
+    interface AddFunction {
+      (a: number, b: number): number;
+    }
+
+    const add: AddFunction = (value1: number, value2: number) => {
+      return value1 + value2;
+    };
+
+    expect(add(2, 2)).toBe(4);
+  });
+
+  it("should support indexable interface", () => {
+    interface StringArray {
+      [index: number]: string;
+    }
+
+    const names: StringArray = ["John", "Doe"];
+    console.info(names);
+    expect(names[0]).toBe("John");
+
+    interface StringDictionary {
+      [key: string]: string;
+    }
+
+    const dictionary: StringDictionary = {
+      name: "John Doe",
+      age: "30",
+    };
+
+    console.info(dictionary);
+    expect(dictionary["name"]).toBe("John Doe");
+  });
+
+  it("should support extends interface", () => {
+    const Udin: Employee = {
+      id: 1,
+      name: "Udin",
+      division: "IT",
+    };
+
+    const Elmo: Manager = {
+      id: "M1",
+      name: "Elmo",
+      division: "IT",
+      numberOfEmployees: 10,
+    };
+
+    console.info(Udin);
+    console.info(Elmo);
+  });
+});
