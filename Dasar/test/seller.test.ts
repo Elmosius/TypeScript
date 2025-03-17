261d0562c61b0aa441f13d7a0a66275b82fc0a97
Elmosius <2272008@maranatha.ac.id>
2025-03-17 21:15:04 +0700

    coba function interface

--- Dasar/test/seller.test.ts
+++ Dasar/test/seller.test.ts
@@ -13,4 +13,16 @@ describe("Interface", () => {
     // seller.phone = "123";  // gabisa karena readonly
     console.info(seller);
   });
+
+  it("should support function interface in typescript", () => {
+    interface AddFunction {
+      (a: number, b: number): number;
+    }
+
+    const add: AddFunction = (value1: number, value2: number) => {
+      return value1 + value2;
+    };
+
+    expect(add(2, 2)).toBe(4);
+  });
 });
