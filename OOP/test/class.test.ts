a5a27d6739eaac5a14884a2c2f8e2c1b5984cc91
Elmosius Suli <elmosius18@gmail.com>
2025-03-18 21:35:10 +0700

    coba buat class di tsc
    mirip dengan di javascript

---
+++ OOP/test/class.test.ts
@@ -0,0 +1,14 @@
+describe('Class', () => {
+    it('should can be create class', () => {
+        class Customer{
+
+        }
+
+        class Order{
+
+        }
+
+        const customer: Customer = new Customer();
+        const order: Order = new Order();
+    });
+});
